34a5ab45852a698db722a0abb12a8b7eef278449
Oscar Santos Ortiz <santosortizoscar.itz@outlook.com>
2021-06-07 09:11:15 -0500

    Final Oscar

---
+++ frontend/src/app/components/lgac/lgac.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lgac',
+  templateUrl: './lgac.component.html',
+  styleUrls: ['./lgac.component.css']
+})
+export class LGACComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ frontend/src/app/components/normas-estatutos/normas-estatutos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-normas-estatutos',
+  templateUrl: './normas-estatutos.component.html',
+  styleUrls: ['./normas-estatutos.component.css']
+})
+export class NormasEstatutosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
